25e2c6a1f87a3b75d2c4ed2e68a28a45848bf074
jsaroha <jyotsnasaroha@gmail.com>
2016-10-14 19:25:37 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,4 +62,13 @@ creates => '/etc/motd',
 path => '/usr/bin:/usr/local/bin',
 }
 
+yumrepo { 'updates':
+  ensure     => 'present',
+  descr      => 'CentOS-$releasever - Updates',
+  enabled    => '1',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+}
+
 }
